ff548031461e2b5fb16ea065bb0b82a4592a9984
Michael Mouton <michael.mouton510@gmail.com>
2025-04-12 10:24:52 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,11 +5,12 @@
 #IMGSRCDIR=cat-images/
 
 PROTOCOL=
-FQDN=
-IMGSRCDIR=cat-images/
+FQDN=.
+IMGSRCDIR=
 
 mkdir -p ./output/html
 cp ./cat-images/*.jpg ./output/html
+cp ./source/errorstyle.css ./output/html
 touch ./output/cat-images.conf
 
 cat ./source/apache2_template | sed -e "s%\[IMGSRCDIR\]%$IMGSRCDIR%g" | tee -a ./output/cat-images.conf > /dev/null
@@ -19,7 +20,7 @@ for CATIMG in ./cat-images/*.jpg
 do
 	ERROR_NUM=$(echo $CATIMG | awk -F/ '{print $3}' | awk -F. '{print $1}')
 
-	cat ./source/html_template | sed -e "s%\[ERROR_NUM\]%$ERROR_NUM%g" -e "s%\[PROTOCOL\]%$PROTOCOL%" -e "s%\[FQDN\]%$FQDN%" -e "s%\[IMGSRCDIR\]%$IMGSRCDIR%" | tee ./output/html/$ERROR_NUM.html > /dev/null
+	cat ./source/html_template | sed -e "s%\[ERROR_NUM\]%$ERROR_NUM%g" -e "s%\[PROTOCOL\]%$PROTOCOL%g" -e "s%\[FQDN\]%$FQDN%g" -e "s%\[IMGSRCDIR\]%$IMGSRCDIR%g" | tee ./output/html/$ERROR_NUM.html > /dev/null
 
 	echo "ErrorDocument $ERROR_NUM /$IMGSRCDIR$ERROR_NUM.html" | tee -a ./output/cat-images.conf > /dev/null
 done
